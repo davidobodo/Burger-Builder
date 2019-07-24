b8e94140ad7ed04025c738c370fc60c51b56acc7
David Obodo <obododavid5@gmail.com>
2019-07-24 08:12:25 +0100

    navigating to checkout page successful

--- src/containers/BurgerBulider/BurgerBuilder.js
+++ src/containers/BurgerBulider/BurgerBuilder.js
@@ -109,27 +109,28 @@ class BurgerBuilder extends Component{
     }
 
     purchaseContinueHandler =() => {
-        this.setState({loading:true})//so that i would see spinner
+        // this.setState({loading:true})//so that i would see spinner
         
-        const order ={
-            ingredients:this.state.ingredients,
-            price:this.state.totalPrice,
-            customer : {
-                name: 'Obodo David',
-                address:{
-                    street: 'RCCG camp',
-                    zipCode: '12345',
-                    country: 'Nigeria'
-                },
-                email: 'obodo@test.com'    
-                },
-            deliveryMethod : 'fastest'
-        }
-        axios.post('/orders.json', order )
-            .then(response => 
-                this.setState({loading:false, purchasing:false}))
-            .catch(error =>
-                this.setState({loading:false, purchasing:false}));
+        // const order ={
+        //     ingredients:this.state.ingredients,
+        //     price:this.state.totalPrice,
+        //     customer : {
+        //         name: 'Obodo David',
+        //         address:{
+        //             street: 'RCCG camp',
+        //             zipCode: '12345',
+        //             country: 'Nigeria'
+        //         },
+        //         email: 'obodo@test.com'    
+        //         },
+        //     deliveryMethod : 'fastest'
+        // }
+        // axios.post('/orders.json', order )
+        //     .then(response => 
+        //         this.setState({loading:false, purchasing:false}))
+        //     .catch(error =>
+        //         this.setState({loading:false, purchasing:false}));
+            this.props.history.push('/Checkout')
     } 
 
 

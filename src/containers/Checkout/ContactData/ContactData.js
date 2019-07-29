4db63a1f675d189b9cbaf0efab69bf596decfc25
David Obodo <obododavid5@gmail.com>
2019-07-29 13:18:08 +0100

    fixed littel bug with validation of dropdown

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -86,6 +86,7 @@ class ContactData extends Component{
                         {value:'Cheapest', displayValue:'Cheapest'}
                     ]
                 },
+                validation:{},
                 value:'',
                 valid:true
             },
@@ -96,6 +97,10 @@ class ContactData extends Component{
 
     checkValidity = (value, rules) => {
         let isValid = true;
+
+        if(!rules){
+            return true;
+        }
 
         if(rules.required){
             isValid = value.trim() !== '' && isValid;

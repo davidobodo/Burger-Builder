14266e8134029ef582f5937906ef58019560d8c8
David Obodo <obododavid5@gmail.com>
2019-08-02 23:04:58 +0100

    made my order buttton work(can be done two ways ...stuck to the non redux way)

--- src/containers/BurgerBulider/BurgerBuilder.js
+++ src/containers/BurgerBulider/BurgerBuilder.js
@@ -18,7 +18,6 @@ class BurgerBuilder extends Component{
     //the amount of each ingredient present in the burger
     state={
         //sole aim of purchaseable is to enable the ORDER NOW button
-        purchasable: false,
         purchasing: false,
         loading:false,
         error:false
@@ -44,7 +43,7 @@ class BurgerBuilder extends Component{
             .reduce((sum, el) => {
                 return sum + el;
             }, 0);
-        this.setState({purchasable: sum > 0})
+        return sum > 0;
     }
 
     //function that gets called when i want to add an ingredient
@@ -148,7 +147,7 @@ class BurgerBuilder extends Component{
                             ingredientAdded={this.props.onIngredientAdded}
                             ingredientRemoved={this.props.onIngredientRemoved}
                             disabled={disabledInfo}
-                            purchasable={this.state.purchasable}
+                            purchasable={this.updatePurchaseState(this.props.ings)}
                             ordered={this.purchaseHandler}
                             price={this.props.price}/>
                     </Auxi>

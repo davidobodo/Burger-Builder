a9b3e0cfd211394b4060f9686f960c0173d30ada
David Obodo <obododavid5@gmail.com>
2019-08-05 13:37:37 +0100

    using action creators to handle addition and removal of ingredients

--- src/containers/BurgerBulider/BurgerBuilder.js
+++ src/containers/BurgerBulider/BurgerBuilder.js
@@ -8,7 +8,7 @@ import axios from '../../axios(orders)'
 import Spinner from '../../components/UI/Spinner/Spinner'
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler'
 import {connect} from 'react-redux'
-import * as actionTypes from '../../store/actions'
+import * as burgerBuilderActions from '../../store/actions/rootActions'
 
 
 //Prices for each ingredient
@@ -184,8 +184,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return{
-        onIngredientAdded: (ingName) => dispatch({type: actionTypes.ADD_INGREDIENT, ingredientName: ingName}),
-        onIngredientRemoved: (ingName) => dispatch({type: actionTypes.REMOVE_INGREDIENT, ingredientName:ingName})
+        onIngredientAdded: (ingName) => dispatch(burgerBuilderActions.addIngredient(ingName)),
+        onIngredientRemoved: (ingName) => dispatch(burgerBuilderActions.removeIngredient(ingName))
     }
 }
 
---
+++ src/store/actions/rootActions.js
@@ -0,0 +1,7 @@
+export {
+    addIngredient,
+    removeIngredient
+} from './burgerBuilderActions';
+
+
+export {} from './orderActions'

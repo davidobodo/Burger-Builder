2f132d1902510fcc92c3551cb758d0f5b0203e3b
David Obodo <obododavid5@gmail.com>
2019-08-07 19:52:56 +0100

    finished hooking my orders component to redux

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -2,29 +2,44 @@ import React,{ Component } from 'react'
 import Order from '../../components/Order/Order'
 import axios from '../../axios(orders)'
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler'
+import * as actions from '../../store/actions/rootActions'
+import { connect } from 'react-redux'
+import Spinner from '../../components/UI/Spinner/Spinner'
 
 class Orders extends Component{
     //Note that anything that is dynamic in a page has to be in a state, so that it can  change...hence the initialization of this state
-    state={
-        orders: [],
-        loading:true
+
+    componentDidMount(){
+        this.props.onFetchOrders();
     }
     
 
     render(){
-        return(
-            <div>
-                {this.state.orders.map(order => (
-                    
+        let orders = <Spinner/>
+        if(!this.props.loading){
+            orders = this.props.orders.map(order => ( 
                     <Order 
                         key={order.id}
                         ingredients={order.ingredients}
                         price={order.price}/>//adding + here is another option for turning the string price into a number(although i already used number.parsefloat in  the order component to do that)
-                ))}
-            </div>
-
-        )   
+                ))
+            
+        }
+        return orders   
     }
 }
 
-export default withErrorHandler(Orders, axios)
+const mapStateToProps = state => {
+    return{
+        orders: state.order.orders,
+        loading: state.order.loading
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return{
+        onFetchOrders : () => dispatch(actions.fetchOrders())
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(withErrorHandler(Orders, axios))

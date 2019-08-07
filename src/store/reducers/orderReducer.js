306f41dd0e5a59892fc39bef39133c9d65160f85
David Obodo <obododavid5@gmail.com>
2019-08-07 20:14:53 +0100

    used updated object on orderReducer

--- src/store/reducers/orderReducer.js
+++ src/store/reducers/orderReducer.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes'
+import { updatedObject } from '../utility'
 
 const initialState = {
     orders: [],
@@ -9,48 +10,27 @@ const initialState = {
 const reducer = (state=initialState, action) => {
     switch(action.type){
         case actionTypes.PURCHASE_INIT:
-            console.log("redirect")
-            return{
-                ...state,
-                purchased: false
-            }
+            return updatedObject(state, { purchased:false})
         case actionTypes.PURCHASE_BURGER_START:
-            return{
-                ...state,
-                loading:true
-            }
+            return updatedObject(state, { loading:true })
         case actionTypes.PURCHASE_BURGER_SUCCESS:
-            const newOrder = {
-                ...action.orderData,
-                id : action.orderId,
-            }
-            return{
-                ...state,
+            const newOrder = updatedObject(action.orderData, { id: action.orderId})
+            return updatedObject(state, {
                 loading:false,
                 purchased : true,
                 orders: state.orders.concat(newOrder)
-            };
+            })
         case actionTypes.PURCHASE_BURGER_FAIL:
-            return {
-                ...state,
-                loading: false
-            };
+            return updatedObject(state, { loading : false })
         case actionTypes.FETCH_ORDERS_START:
-            return{
-                ...state,
-                loading : true
-            }
+            return updatedObject(state, { loading : true })
         case actionTypes.FETCH_ORDERS_SUCCESS:
-            return{
-                ...state,
+            return updatedObject(state, {
                 loading:false,
                 orders : action.orders
-            }
+            })
         case actionTypes.FETCH_ORDERS_FAIL:
-            return{
-                ...state,
-                loading:false
-            }
+            return updatedObject(state, { loading : false })
         default:
             return state;
     }

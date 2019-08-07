4b09d2effa1ceaa4ef953d3be689750d1e136b8b
David Obodo <obododavid5@gmail.com>
2019-08-07 19:35:24 +0100

    added fetch orders to orderReducer

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -9,23 +9,7 @@ class Orders extends Component{
         orders: [],
         loading:true
     }
-    componentDidMount(){
-        axios.get('/orders.json')
-            .then(res =>{
-                const fetchedOrders =[];
-                //key is the id
-                for(let key in res.data){
-                    fetchedOrders.push({
-                        ...res.data[key],
-                        id:key
-                    });
-                }
-                this.setState({loading:false, orders:fetchedOrders});
-            })
-            .catch(err => {
-                this.setState({loading:false});
-            })
-    }
+    
 
     render(){
         return(

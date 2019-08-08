690304dce546e03ed4d93166ca056304d7623ab0
David Obodo <obododavid5@gmail.com>
2019-08-08 17:15:26 +0100

    adding error message from firebase

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
+import Spinner from '../../components/UI/Spinner/Spinner'
 import classes from './Auth.css';
 import * as actions from '../../store/actions/rootActions'
 
@@ -105,7 +106,7 @@ class Auth extends Component {
             } );
         }
 
-        const form = formElementsArray.map( formElement => (
+        let form = formElementsArray.map( formElement => (
             <Input
                 key={formElement.id}
                 elementType={formElement.config.elementType}
@@ -117,8 +118,19 @@ class Auth extends Component {
                 changed={( event ) => this.inputChangedHandler( event, formElement.id )} />
         ) );
 
+        if(this.props.loading){
+            form = <Spinner/>
+        }
+
+        let errorMessage = null;
+        if(this.props.error){
+            errorMessage =(
+                <p>{this.props.error.message}</p>
+            )
+        }
         return (
             <div className={classes.Auth}>
+                {errorMessage}
                 <form onSubmit={this.submitHandler}>
                     {form}
                     <Button btnType="Success">SUBMIT</Button>
@@ -131,6 +143,13 @@ class Auth extends Component {
     }
 }
 
+const mapStateToProps = state => {
+    return{
+        loading : state.auth.loading,
+        error : state.auth.error
+    }
+}
+
 
 const mapDispatchToProps = dispatch => {
     return {
@@ -138,4 +157,4 @@ const mapDispatchToProps = dispatch => {
     };
 };
 
-export default connect(null, mapDispatchToProps)(Auth);
+export default connect(mapStateToProps, mapDispatchToProps)(Auth);

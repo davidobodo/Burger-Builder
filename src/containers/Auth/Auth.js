f355318c26d689a5e4eaf1fcf3f4a492633bef5d
David Obodo <obododavid5@gmail.com>
2019-08-08 13:18:38 +0100

    incorporated signup into actions

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -87,7 +87,7 @@ class Auth extends Component {
 
     submitHandler = (event) => {
         event.preventDefault();
-        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value);
+        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value, this.state.isSignUp);
     }
 
     switchAuthModeHandler = () => {
@@ -134,7 +134,7 @@ class Auth extends Component {
 
 const mapDispatchToProps = dispatch => {
     return {
-        onAuth: (email, password) => dispatch(actions.auth(email, password))
+        onAuth: (email, password, isSignUp) => dispatch(actions.auth(email, password,  isSignUp))
     };
 };
 

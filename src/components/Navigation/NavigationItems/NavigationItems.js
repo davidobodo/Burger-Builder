6d8b5f5549ce4503607acddc9f729a68df2e2671
David Obodo <obododavid5@gmail.com>
2019-09-17 23:20:52 +0100

    made sure orders link is not shown when user is not authenticated

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -7,7 +7,7 @@ const navigationItems = (props) => {
         
             <ul className={classes.NavigationItems}>
                 <NavigationItem link="/" exact>BurgerBuilder</NavigationItem>
-                <NavigationItem link="/orders">Orders</NavigationItem>
+                {props.isAuthenticated ?<NavigationItem link="/orders">Orders</NavigationItem>: null}
                 {!props.isAuthenticated 
                     ? <NavigationItem link="/auth">Authenticate</NavigationItem>
                     : <NavigationItem link="/logout">Logout</NavigationItem>}
